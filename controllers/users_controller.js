84970a5f70865789445e31be42b53409a85047ba
Sidhak <singhsidhak36@gmail.com>
2019-07-15 04:28:54 +0530

    Add authentication via sign in and sign out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,9 +3,30 @@
 const Users = require('../models/userSchema');
 
 module.exports.profilePage = function(req, res){
-    return res.render('user_profile', {
-        title : "User's Profile"
-    });
+
+    if(req.cookies.user_id){
+        //Handle user signed in
+        Users.findById(req.cookies.user_id, function(err, user){
+            if(err){
+                console.log('An error occured while finding user by id');
+                return;
+            }
+
+            //Handle user found
+            if(user){
+                return res.render('user_profile', {
+                    title : "User's Profile",
+                    user: user
+                });
+            }
+            //Handle user not found
+            return res.redirect('/users/sign-in');
+        });
+    }else{
+        //Handle user not signed in
+        return res.redirect('/users/sign-in');
+    }
+
 }
 
 module.exports.info = function(req, res){
@@ -38,7 +59,7 @@ module.exports.create = function(req, res){
 
     Users.findOne({email: req.body.email}, function(err, user){
         if(err){
-            console.log('An error occured while finding user with specified email');
+            console.log('An error occured while finding user during signup');
             return;
         }
 
@@ -59,5 +80,32 @@ module.exports.create = function(req, res){
 
 //sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    //TODO Later
+    
+    //find the user
+    Users.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('An error occured while finding user during sign in');
+            return;
+        }
+
+        //handle user found
+        if(user){
+            //handle password which don't match
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            //handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        }else{
+            //handle user not found
+            return res.redirect('back');
+        }
+    });
+}
+
+module.exports.signOut = function(req, res){
+    res.cookie('user_id', "");
+    return res.redirect('/users/sign-in');
 }
